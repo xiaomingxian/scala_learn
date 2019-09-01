fb124340dced2741a315f13d8ca4fb739586c344
xxm <934792834@qq.com>
2019-09-01 22:25:38 +0800

    基础

--- src/main/scala/com/xxm/T2_Base2.scala
+++ src/main/scala/com/xxm/T2_Base2.scala
@@ -19,11 +19,47 @@ object T2_Base2 {
     println(s"\nread context:$name,double:$price")
   }
 
+  def for_demo: Unit = {
+    for (i <- 1 to 3) {
+      //前后闭合
+      println("---->context:", i)
+    }
+
+    for (i <- 1 until 3) {
+      println("---<前闭后开>---", i)
+    }
+
+    println("---------------多层for循环-----------")
+    var ii = 0
+    for (i <- 1 to 3; j <- 5 to 10; k <- 1 to 4) {
+      //i*j
+      //println("--->i:" + i)
+      //println("--->j:" + j)
+      ii += 1
+    }
+    println("~~~~~~次数：" + ii)
+    println("-=-==" * 7)
+    var list = List(1, 2, "v1", "v2")
+    for (l <- list) println(l)
+    println("-=-~~" * 7)
+    //  yield 接收保存生成方式
+    var a = 0
+    var yield_val = for {
+      a
+        <- List(1, 2, 3, 4, 5, 6, 7)
+      if a < 6;if a > 2
+    } yield a
+
+    for (aa <- yield_val) println("========>:" + aa)
+  }
+
   def main(args: Array[String]): Unit = {
     //1.demo
     trait_demo
     //2.console-demo
-    console_demo
+    //console_demo
+    //3.for循环
+    for_demo
   }
 }
 //特质：综合了 接口与抽象类
