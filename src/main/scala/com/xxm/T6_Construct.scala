2722221600af5a6ac1f557dcc11552e9199428be
xxm <934792834@qq.com>
2019-09-11 22:26:54 +0800

    构造器

---
+++ src/main/scala/com/xxm/T6_Construct.scala
@@ -0,0 +1,58 @@
+package com.xxm
+
+/**
+  * 主构造器和辅助构造器
+  */
+object Construct {
+  def main(args: Array[String]): Unit = {
+    val a = new A("Tom", 10)
+    println(a)
+    val aa = new A("tom")
+    println(aa)
+    println("～～～～～～～～～～～～～～～～～～～～～～")
+    //  测试 显示调用父类是为了调用其父类
+    val son = new Son2("tom")
+
+  }
+
+}
+
+//主构造器
+class A(name: String, age: Int) {
+
+  var name2: String = name
+  var age2 = age
+  this.age2 += 10
+
+  //函数以外的部分都会在构造函数中(主构造器中)
+  println("-------before--------" + this.age2)
+
+  override def toString: String = name2 + "  ---   " + age2
+
+
+  //辅助构造器1---每个辅助构造器都必须以一个对先前已经定义的其他辅助器或者主构造器的调用开始
+  //在底层就是java的构造器重载
+  def this(name: String) {
+    this(name, 22) //以主构造器为基准(直接或间接的调用主构造器)----得显示的构造主构造器是为了调用其父类
+    this.name2 = name;
+  }
+
+  println("-------after--------")
+  //辅助构造器2
+  //def this(name: String, age: Int) {
+  //  this(name)
+  //  this.age2 = age
+  //}
+
+}
+
+class Father2 {
+  println("=======父类")
+}
+
+class Son2 extends Father2 {
+  def this(name: String) {
+    this
+    println("-----子类")
+  }
+}
