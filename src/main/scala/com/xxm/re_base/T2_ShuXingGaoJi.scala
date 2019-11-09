71cc18be85a9485f8232f0ea44641c7da026f76b
xxm <934792834@qq.com>
2019-11-09 23:26:20 +0800

    对象属性高级特性

--- src/main/scala/com/xxm/re_base/T2_ShuXingGaoJi.scala
+++ src/main/scala/com/xxm/re_base/T2_ShuXingGaoJi.scala
@@ -1,8 +1,18 @@
 package com.xxm.re_base
 
-class T2_ShuXingGaoJi {
+import scala.beans.BeanProperty
+
+object T2_ShuXingGaoJi {
 
   def base: Unit = {
+    var work = new Work("构造方法属性不可见")
+    work.getSex
+    work.setSex(1)
+    work.sex
+    work.sex = 2
+    new Work2("构造方法属性只读").inName //=""
+    new Work3("构造方法属性可赋值").inName = "可变化"
+
 
 
   }
@@ -16,6 +26,9 @@ class T2_ShuXingGaoJi {
 
 //构造函数中的属性不可见  Work(inName: String)
 class Work(inName: String) {
+
+  //会生成get/set方法 原来的属性get/set也可用
+  @BeanProperty var sex: Int = _
   var name: String = inName
 }
 
