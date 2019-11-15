fc32e9f516b156369eda307077caae11165a9c8e
xxm <934792834@qq.com>
2019-11-16 00:10:04 +0800

    混入继承类/继承类(得是特质超类的子类)又混入特质

--- src/main/scala/com/xxm/z_height/T4_TraitDemo2.scala
+++ src/main/scala/com/xxm/z_height/T4_TraitDemo2.scala
@@ -2,8 +2,15 @@ package com.xxm.z_height
 
 object T4_TraitDemo2 {
   def main(args: Array[String]): Unit = {
-    //
+    //xxm1::声明时混入 --- 当前类最后初始化
+    println("---------------->>声明时混入")
     new FF()
+    println("---------------->>构造时混入")
+    //xxm2::构造时混入 --- 当前类最先初始化
+    new EE with BB with CC
+
+    //xxm3::特质可以继承类  / 如果混入该特质的类，已经继承了另一个类(A类)，则要求A类必须是特质超类的子类，否则就会出现多继承，报错
+
   }
 }
 
